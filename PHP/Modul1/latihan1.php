53ad5918b2ed2aa05302af62d7d3127980a9f9c7
DemsiRakhi <42015138+DemsiRakhi@users.noreply.github.com>
2019-02-19 23:32:56 -0800

    Add files via upload

---
+++ PHP/Modul1/latihan1.php
@@ -0,0 +1,5 @@
+<?php
+echo "Namaku Rakhi Ismed";
+echo "<br>";
+echo "Aku sedang belajar PHP di SMK Telkom Malang";
+?>
---
+++ PHP/Modul2/latihan7.php
@@ -0,0 +1,17 @@
+<?php
+
+$buah = array ("Mangga", "Aple", "Pisang", "Jeruk");
+echo $buah[0];
+echo "<br>";
+echo $buah[3];
+
+$warna = array();
+$warna[] = "Merah";
+$warna[] = "Biru";
+$warna[] = "Hijau";
+$warna[] = "Putih";
+echo "<br>";
+echo $warna[0]; 
+echo "<br>";
+echo $warna[2];
+?>
